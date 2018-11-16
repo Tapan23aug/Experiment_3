93884a38912f557fe4c4272bc812ccbfee332ee2
Tapan <tapan23aug@gmail.com>
2018-11-16 23:13:55 +0530

    intial

---
+++ app/src/main/java/com/example/tapan/experiment_3/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.tapan.experiment_3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.tapan.experiment_3.R;
+
+public class MainActivity extends AppCompatActivity {
+    EditText edt;
+    String str;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        edt=(EditText)findViewById(R.id.editText);
+
+    }
+
+    public void ok(View view) {
+        str=edt.getText().toString();
+        Toast.makeText(getApplicationContext(),str,Toast.LENGTH_SHORT).show();
+    }
+}
